aaaa921e8e499eac3a57eeeaa3592ac5f7a7f107
RishikBacancy <rishik.karanjiya@bacancy.com>
2022-03-09 19:58:07 +0530

    setup Screens

---
+++ screens/CategoriesMealsScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, StyleSheet} from "react-native";
+
+const CategoriesMealsScreen = () =>
+{
+  return(
+    <View>
+      <Text>CategoriesMealsScreen Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+});
+
+export default CategoriesMealsScreen;
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, StyleSheet} from "react-native";
+
+const CategoriesScreen = () =>
+{
+  return(
+    <View>
+      <Text>CategoriesScreen Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+});
+
+export default CategoriesScreen;
