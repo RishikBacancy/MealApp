ae8a6c5343c00419d65223f117d3a7df781dc2f5
RishikBacancy <rishik.karanjiya@bacancy.com>
2022-03-14 12:03:10 +0530

    Header Button Added!

--- components/HeaderButton.js
+++ components/HeaderButton.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Text, StyleSheet, View, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import Colors from '../constants/Colors';
 
 const HeaderButton = (props) => {
 	return (
 		<View>
 			<Icon 
-                onPress={props.selectedBtn} 
-                name={props.iconType} 
-                size={23} 
-                color={props.iconColor}
-                />
+                                onPress={props.selectedBtn} 
+                                name={props.iconType} 
+                                size={23} 
+                                color={props.iconColor}
+                        />
 		</View>
 	);
 };
