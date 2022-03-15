de27a68c6c6c3cae3523925da9e6a2bb81583b33
RishikBacancy <rishik.karanjiya@bacancy.com>
2022-03-15 16:24:11 +0530

    Display Ingredients and steps in Detail Screen!

--- src/screens/MealDetailsScreen.js
+++ src/screens/MealDetailsScreen.js
@@ -1,45 +1,85 @@
-import React, {useEffect} from "react";
-import { View, Text, StyleSheet, Platform} from "react-native";
-import { MEALS } from "../data/dummy-data";
-import HeaderButton from "../components/HeaderButton";
-import Colors from "../constants/Colors";
+import React, { useEffect } from 'react';
+import { ScrollView, Image, View, Text, StyleSheet, Platform } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
+import Colors from '../constants/Colors';
+import DefaultText from '../components/DefaultText';
 
-const MealDetailsScreen = props =>
+const ListDetail = props =>
 {
-
-  const mealId = props.route.params;
-
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
-
-  useEffect(() => {
-    props.navigation.setOptions({
-      title: selectedMeal.title,
-      headerRight: () => (
-        <HeaderButton 
-          iconType ="heart"
-          iconColor = {Platform.OS === "android" ? "white" : Colors.primaryColor}
-          selectedBtn = {()=> (console.log("btn Pressed!"))}>
-        </HeaderButton>
-      ),
-    });
-  },[selectedMeal])
-
   return(
-    <View style={styles.screen}>
-      <Text style={styles.textWrap}>{selectedMeal.title}</Text>
+    <View style={styles.listDetailContainer}>
+      <DefaultText>{props.children}</DefaultText>
     </View>
   );
-}
+};
+
+const MealDetailsScreen = props => {
+	const mealId = props.route.params;
+
+	const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+	useEffect(
+		() => {
+			props.navigation.setOptions({
+				title: selectedMeal.title,
+				headerRight: () => (
+					<HeaderButton
+						iconType="heart"
+						iconColor={Platform.OS === 'android' ? 'white' : Colors.primaryColor}
+						selectedBtn={() => console.log('btn Pressed!')}
+					/>
+				)
+			});
+		},
+		[ selectedMeal ]
+	);
+
+	return (
+		<ScrollView>
+			<Image source={{ uri: selectedMeal.imageUrl }} style={styles.imgContainer} />
+			<View style={styles.detailsContainer}>
+				<DefaultText>{selectedMeal.duration}m</DefaultText>
+				<DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+				<DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+			</View>
+      <Text style={styles.titleContainer}>Ingredients</Text>
+      
+      {selectedMeal.ingredients.map(ingredient=>
+        <ListDetail key={ingredient}>{ingredient}</ListDetail>
+      )}
+     
+      <Text style={styles.titleContainer}>Steps</Text>
+
+      {selectedMeal.steps.map(step=>
+        <ListDetail key={step}>{step}</ListDetail>
+      )}
+
+		</ScrollView>
+	);
+};
 
 const styles = StyleSheet.create({
-  screen:{
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center",
+	imgContainer:{
+    width:"100%",
+    height:200,
   },
-  textWrap:{
-    fontFamily:"JosefinSans-Regular",
-    fontSize:18,
+  detailsContainer:{
+    flexDirection:"row",
+    padding:15,
+    justifyContent:"space-around",
+  },
+  titleContainer:{
+    fontFamily:"JosefinSans-Bold",
+    fontSize:20,
+    textAlign:"center",
+  },
+  listDetailContainer:{
+    marginVertical:10,
+    marginHorizontal:20,
+    borderColor:"#ccc",
+    borderWidth:1,
+    padding:10,
   },
 });
 
